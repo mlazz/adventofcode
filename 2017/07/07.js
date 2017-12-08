e0e69eb379ac1d17407b61007c0d9cf1646d65f2
Blaz Malezic <171152+mlazz@users.noreply.github.com>
2017-12-08 09:24:54 +0100

    Day 7, part 2

--- 2017/07/07.js
+++ 2017/07/07.js
@@ -1,27 +1,86 @@
-const init = input => {
-    const programs = input.split('\n');
-    const names = [];
-    const subs = [];
-    let parents = programs.filter(p => p.split(' -> ').length > 1);
+var init = input => {
+    // Part 1
+    const map = {},
+        parents = [],
+        allChildren = [];
 
-    parents.forEach(p => {
-        const split = p.split(/ \(\d+\) -> /);
-        names.push(split[0]);
-        subs.push(...split[1].split(', '));
+    input.split('\n').forEach(p => {
+        const matches = p.match(/([\D+]+) \((\d+)\)(?: -> )?([A-Za-z, ]+)?/);
+        if (!matches) {
+            return;
+        }
+
+        const name = matches[1],
+            children = matches[3] && matches[3].split(', ');
+
+        map[name] = {
+            weight: parseInt(matches[2], 10),
+            children: children
+        };
+
+        children && parents.push(name);
+        children && allChildren.push(...children);
     });
 
     let first = false;
-    names.forEach(n => {
-        if (subs.indexOf(n) === -1) {
+    parents.forEach(n => {
+        if (!n.children) {
+            false;
+        }
+
+        if (!allChildren.includes(n)) {
             first = n;
             return;
         }
     });
 
-    console.log('part1: ', first);
+    console.log('part1:', first);
 
-    // recursively check weight
+    // Part 2
+    const getDiff = () => {
+        let found = false;
 
+        const getWeightRecursive = (name) => {
+            const weight = [];
+            const result = {};
+            if (map[name].children) {
+                map[name].children.forEach(c => {
+                    const w = getWeightRecursive(c);
+                    result.diff = w.diff;
+                    result.unbalanced = w.unbalanced;
+
+                    weight.push(w.total);
+                });
+
+                const max = Math.max(...weight),
+                    min = Math.min(...weight);
+                let diff = Math.max(...weight) - Math.min(...weight);
+
+                if (diff !== 0 && !found) {
+                    result.unbalanced = map[name].children[weight.indexOf(max)];
+                    result.diff = diff;
+
+                    found = {
+                        program: result.unbalanced,
+                        weight: map[result.unbalanced].weight,
+                        diff: diff,
+                        corrected: map[result.unbalanced].weight - diff
+                    };
+                }
+            }
+            
+            weight.push(map[name].weight);
+            result.total = weight.length === 1 ? weight[0] : weight.reduce((a, b) => a + b);
+
+            return result;
+        };
+
+        getWeightRecursive(first);
+        return found;
+    }
+
+    const diff = getDiff();
+    console.log('part2:', diff.corrected);
 };
 
 const openFile = (filename, callback) => {
